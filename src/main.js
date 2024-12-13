98eb6595de289f1ca33b611dd7c810b07b7775bf
rezha4 <rezha.bahari@gmail.com>
2024-12-13 16:49:03 +0700

    scens

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
+import Phaser from "./lib/phaser.js";
+import { SCENE_KEYS } from "./scenes/scene-keys.js";
+import { PreloadScene } from "./scenes/preload-scene.js";
+
 const game = new Phaser.Game({
-  parent: "game-container",
-  
+  type: Phaser.CANVAS,
+  pixelArt: false,
+  scale: {
+    parent: "game-container",
+    width: 1024,
+    height: 576
+  },
+  backgroundColor: "#000000"
 });
+
+game.scene.add(SCENE_KEYS.PRELOAD_SCENE, PreloadScene);
